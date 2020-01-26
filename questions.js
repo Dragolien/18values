a8a28957b89c5a9b21648a6075f1589b19f23707
Dragolien <dragolien@hotmail.co.uk>
2020-01-26 18:36:15 +0000

    edited for 9 axes

--- questions.js
+++ questions.js
@@ -2,10 +2,24 @@ questions = [
     {
         "question": "It is necessary for the government to intervene in the economy to protect consumers.",
         "effect": {
-            "econ": 10,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 0
+            "left_1": 0,
+            "right_1": 0,
+            "left_2": 0,
+            "right_2": 0,
+            "left_3": 0,
+            "right_3": 0,
+            "left_4": 0,
+            "right_4": 0,
+            "left_5": 0,
+            "right_5": 0,
+            "left_6": 0,
+            "right_6": 0,
+            "left_7": 0,
+            "right_7": 0,
+            "left_8": 0,
+            "right_8": 0,
+            "left_9": 0,
+            "right_9": 0,
         }
     }
 ];
