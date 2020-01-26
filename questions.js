967b7b3fd3f62228f584a05d547c45203e95cd30
Dragolien <dragolien@hotmail.co.uk>
2020-01-26 21:35:48 +0000

    Update questions.js

--- questions.js
+++ questions.js
@@ -21,5 +21,28 @@ questions = [
             "left_9": 50,
             "right_9": 50,
         }
+    },
+    {
+        "question": "The freer the markets, the freer the people.",
+        "effect": {
+            "left_1": 20,
+            "right_1": 20,
+            "left_2": 30,
+            "right_2": 30,
+            "left_3": 10,
+            "right_3": 10,
+            "left_4": 10,
+            "right_4": 20,
+            "left_5": 30,
+            "right_5": 20,
+            "left_6": 10,
+            "right_6": 20,
+            "left_7": 30,
+            "right_7": 10,
+            "left_8": 20,
+            "right_8": 40,
+            "left_9": 50,
+            "right_9": 50,
+        }
     }
 ];
